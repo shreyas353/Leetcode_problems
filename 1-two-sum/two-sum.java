ad68d372a92b3ce0d1b05ba8d7dba7753008731c
shreyas353 <shreyasgupta1707@gmail.com>
2025-07-14 16:12:25 +0530

    Time: 47 ms (13.05%) | Memory: 44.6 MB (97.96%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,32 +1,17 @@
-public class Solution {
-
-    public int[] twoSum(int[] nums, int target) {
-        // Brute-force approach using two for-loops
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                // Check if sum of two elements equals target
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+class Solution
+{
+    public int[] twoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] {i, j};
                 }
             }
         }
-        // If no pair found
-        return new int[] { -1, -1 };
-    }
-
-    // Main function to test the above code
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-
-        int[] result = sol.twoSum(nums, target);
-
-        if (result[0] != -1) {
-            System.out.println("Indices: " + result[0] + ", " + result[1]);
-        } else {
-            System.out.println("No two numbers add up to the target.");
-        }
+        throw new IllegalArgumentException("No match");
     }
 }
