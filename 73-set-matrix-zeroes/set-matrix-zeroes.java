cff9a4b02f85f24a94c1b6988f20ca2e42d99300
shreyas353 <shreyasgupta1707@gmail.com>
2025-08-07 21:26:49 +0530

    Time: 2 ms (17.72%) | Memory: 45.6 MB (66.98%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,16 +1,17 @@
-class Solution {
+class Solution 
+{ 
     public void setZeroes(int[][] matrix) 
     {
-        boolean rows[] = new boolean[matrix.length];
-        boolean cols[] = new boolean[matrix[0].length];
+        Set<Integer> rowsSet=new HashSet<>();
+        Set<Integer> colsSet=new HashSet<>();
         for(int i=0;i<matrix.length;i++)
         {
             for(int j=0;j<matrix[0].length;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    rows[i] = true;
-                    cols[j] = true;
+                    rowsSet.add(i);
+                    colsSet.add(j);
                 }
             }
         }
@@ -18,7 +19,7 @@ class Solution {
         {
             for(int j=0;j<matrix[0].length;j++)
             {
-                if(rows[i]||cols[j])
+                if(rowsSet.contains(i)||colsSet.contains(j))
                 {
                     matrix[i][j]=0;
                 }
@@ -26,3 +27,32 @@ class Solution {
         }
     }
 }
+// class Solution 
+// {
+//     public void setZeroes(int[][] matrix) 
+//     {
+//         boolean rows[] = new boolean[matrix.length];
+//         boolean cols[] = new boolean[matrix[0].length];
+//         for(int i=0;i<matrix.length;i++)
+//         {
+//             for(int j=0;j<matrix[0].length;j++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     rows[i] = true;
+//                     cols[j] = true;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<matrix.length;i++)
+//         {
+//             for(int j=0;j<matrix[0].length;j++)
+//             {
+//                 if(rows[i]||cols[j])
+//                 {
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+//     }
+// }
