4fd7acb30b0630b8e13139e809e179af9a97496e
shreyas353 <shreyasgupta1707@gmail.com>
2026-02-24 22:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (63.02%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,8 +1,6 @@
-class Solution 
-{
-    public int[][] generateMatrix(int n) 
-    {
-        int a[][]=new int[n][n];
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int arr[][]=new int[n][n];
         int sr=0,sc=0,er=n,ec=n;
         int s=1;
         for(int i=0;i<n;i++)
@@ -13,13 +11,13 @@ class Solution
                 {
                     for (i=sc;i<ec;i++)
                     {
-                        a[sr][i]=s;
+                        arr[sr][i]=s;
                         s=s+1;
                     }
                     sr=sr+1;
                     for (i=sr;i<er;i++)
                     {
-                        a[i][ec-1]=s;
+                        arr[i][ec-1]=s;
                         s=s+1;
                     }
                     ec=ec-1;
@@ -27,7 +25,7 @@ class Solution
                     {
                         for (i=ec-1;i>sc-1;i--)
                         {
-                            a[er-1][i]=s;
+                            arr[er-1][i]=s;
                             s=s+1;
                         }
                         er=er-1;
@@ -36,7 +34,7 @@ class Solution
                     {
                         for (i=er-1;i>sr-1;i--)
                         {
-                            a[i][sc]=s;
+                            arr[i][sc]=s;
                             s=s+1;
                         }
                         sc=sc+1;
@@ -44,23 +42,6 @@ class Solution
                 }
             }
         }
-        return a;
-    }
-    public static void main(String[] args)
-    {
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Enter the size of matrix:");
-        int n=scan.nextInt();
-        Solution solution=new Solution();
-        int matrix[][]=solution.generateMatrix(n);
-        System.out.println("Spiral matrix:");
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                System.out.print(matrix[i][j]+"\t");
-            }
-            System.out.println();
-        }
+        return arr;
     }
 }
