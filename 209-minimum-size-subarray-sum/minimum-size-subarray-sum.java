bb41300531a5016e41169cba1544e47887889871
shreyas353 <shreyasgupta1707@gmail.com>
2026-03-09 19:53:40 +0530

    Time: 1 ms (99.78%) | Memory: 62 MB (85.32%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -8,17 +8,11 @@ class Solution {
         for(high=0;high<nums.length;high++){
             sum=sum+nums[high];
             while(sum>=target){
-                int len=high-low+1;
-                res=Math.min(res,len);
+                res=Math.min(res,high-low+1);
                 sum=sum-nums[low];
                 low++;
             }
         }
-        if(res==Integer.MAX_VALUE){
-            return 0;
-        }
-        else{
-            return res;
-        }
+        return res==Integer.MAX_VALUE ? 0 : res;
     }
 }
