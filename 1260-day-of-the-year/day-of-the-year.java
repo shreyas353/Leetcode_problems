8c0a96238677f71e9a5998bca09d858e3aa98f72
shreyas353 <shreyasgupta1707@gmail.com>
2026-06-21 11:06:16 +0530

    Time: 7 ms (97.36%) | Memory: 46.9 MB (77.70%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.java
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -1,19 +1,16 @@
-import java.time.LocalDate;
 class Solution {
     public int dayOfYear(String date) {
-        LocalDate d=LocalDate.parse(date);
-        return d.getDayOfYear();
-        // int year=Integer.parseInt(date.substring(0,4));
-        // int month=Integer.parseInt(date.substring(5,7));
-        // int day=Integer.parseInt(date.substring(8,10));
-        // int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
-        // if((year%400==0)||(year%4==0&&year%100!=0)){
-        //     days[1]=29;
-        // }
-        // int dayNumber=day;
-        // for(int i=0;i<month-1;i++){
-        //     dayNumber=dayNumber+days[i];
-        // }
-        // return dayNumber;
+        int year=Integer.parseInt(date.substring(0,4));
+        int month=Integer.parseInt(date.substring(5,7));
+        int day=Integer.parseInt(date.substring(8,10));
+        int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+        if((year%400==0)||(year%4==0&&year%100!=0)){
+            days[1]=29;
+        }
+        int dayNumber=day;
+        for(int i=0;i<month-1;i++){
+            dayNumber=dayNumber+days[i];
+        }
+        return dayNumber;
     }
 }
