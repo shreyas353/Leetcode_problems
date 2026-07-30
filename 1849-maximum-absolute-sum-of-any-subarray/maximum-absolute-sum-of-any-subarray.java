712eabb4bbe53ba71198b80bf6972465b965ab8a
shreyas353 <shreyasgupta1707@gmail.com>
2026-07-30 23:50:56 +0530

    Time: 9 ms (17.54%) | Memory: 66.4 MB (96.37%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -6,8 +6,7 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             max_ending=Math.max(max_ending+nums[i],nums[i]);
             min_ending=Math.min(min_ending+nums[i],nums[i]);
-            ans = Math.max(ans,Math.abs(max_ending));
-            ans = Math.max(ans,Math.abs(min_ending));
+            ans=Math.max(ans,Math.max(Math.abs(max_ending),Math.abs(min_ending)));
         }
         return ans;
     }
