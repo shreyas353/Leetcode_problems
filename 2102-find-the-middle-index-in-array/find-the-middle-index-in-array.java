71f57ec769f605baacb39494936f8df550a11ade
shreyas353 <shreyasgupta1707@gmail.com>
2026-08-03 21:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (21.05%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -5,12 +5,15 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             sum=sum+nums[i];
         }
-        for(int i=0;i<nums.length;i++){
+        if(sum-nums[0]==0){
+            return 0;
+        }
+        for(int i=1;i<nums.length;i++){
+            left=left+nums[i-1];
             int right=sum-nums[i]-left;
             if(left==right){
                 return i;
             }
-            left=left+nums[i];
         }
         return -1;
     }
