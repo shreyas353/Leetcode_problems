50939339ce6bf83afaf5408bef7eb14705a1b0b2
shreyas353 <shreyasgupta1707@gmail.com>
2026-08-05 00:39:29 +0530

    Time: 2 ms (98.73%) | Memory: 42.7 MB (28.55%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.java
+++ 728-self-dividing-numbers/self-dividing-numbers.java
@@ -3,16 +3,14 @@ class Solution {
          List<Integer> ans=new ArrayList<>();
         for(int i=left;i<=right;i++){
             int original=i;
-            boolean self=true;
             while(original>0){
                 int digits=original%10;
                 if(digits==0||i%digits!=0){
-                    self=false;
                     break;
                 }
                 original=original/10;
             }
-            if(self){
+            if(original==0){
                 ans.add(i);
             }
         }
