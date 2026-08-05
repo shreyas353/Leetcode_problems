4abcdfd44ebb32202ee84572d9b723b4e791af87
shreyas353 <shreyasgupta1707@gmail.com>
2026-08-05 20:17:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (12.51%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i=0;
+        while(i<bits.length-1){
+            if(bits[i]==0){
+                i++;
+            }
+            else{
+                i=i+2;
+            }
+        }
+        return i==bits.length-1;
+    }
+}
