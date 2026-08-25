8059d57ac2486fd5d15124277e4b7647f98e9a95
shreyas353 <shreyasgupta1707@gmail.com>
2026-08-25 23:49:52 +0530

    Time: 3 ms (77.36%) | Memory: 46.4 MB (74.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,56 +1,42 @@
-class Solution 
-{
-    public int romanToInt(String s) 
-    {
-        char[] chars = s.toCharArray();
-        int result = 0;
-        int i, j;
-        for (i = 0, j = 1; j < chars.length; i++, j++) 
-        {
-            if (valueOf(chars[i]) >= valueOf(chars[j])) 
-            {
-                result = result + valueOf(chars[i]);
-            } 
-            else 
-            {
-                result = result - valueOf(chars[i]);
+class Solution {
+    public int romanToInt(String s) {
+        char[] chars=s.toCharArray();
+        int result=0;
+        int i,j;
+        for(i=0,j=1;j<chars.length;i++,j++){
+            if(valueOf(chars[i])>=valueOf(chars[j])){
+                result=result+valueOf(chars[i]);
+            }
+            else{
+                result=result-valueOf(chars[i]);
             }
         }
-        result = result + valueOf(chars[i]);
+        result = result+valueOf(chars[i]);
         return result;
     }
-    private int valueOf(char ch) 
-    {
-        if (ch == 'I')
-        {
+    private int valueOf(char ch) {
+        if (ch=='I'){
             return 1;
         }
-        else if (ch == 'V')
-        {
+        else if (ch=='V'){
             return 5;
         }
-        else if (ch == 'X')
-        {
+        else if (ch=='X'){
             return 10;
         }
-        else if (ch == 'L')
-        {
+        else if (ch == 'L'){
             return 50;
         }
-        else if (ch == 'C')
-        {
+        else if (ch == 'C'){
             return 100;
         }
-        else if (ch == 'D')
-        {
+        else if (ch == 'D'){
             return 500;
         }
-        else if (ch == 'M')
-        {
+        else if (ch == 'M'){
             return 1000;
         }
-        else
-        {
+        else{
             return 0;
         }
     }
