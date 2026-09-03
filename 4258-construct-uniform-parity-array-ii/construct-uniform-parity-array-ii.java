a92baaf062d1b1983abe94825fb91cb143fd9b11
shreyas353 <shreyasgupta1707@gmail.com>
2026-09-03 19:58:14 +0530

    Time: 8 ms (50.00%) | Memory: 121.7 MB (44.20%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int minOdd = Integer.MAX_VALUE;
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i]%2!=0){
+                minOdd=Math.min(minOdd,nums1[i]);
+            }
+        }
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i]%2==0 && minOdd!=Integer.MAX_VALUE && nums1[i]<minOdd){
+                return false;
+            }
+        }
+        return true;
+    }
+}
