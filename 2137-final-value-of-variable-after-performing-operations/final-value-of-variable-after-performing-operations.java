7850e1b55995689dafd9ac87df40a64791ed60ad
shreyas353 <shreyasgupta1707@gmail.com>
2026-09-09 22:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (68.27%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int x=0;
         for(int i=0;i<operations.length;i++){
-            if(operations[i].contains("+")){
+            if(operations[i].charAt(1)=='+'){
                 x++;
             }
             else{
