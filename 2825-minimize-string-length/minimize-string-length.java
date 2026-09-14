b96d35d11152db498aef4125679ce8980754d78b
shreyas353 <shreyasgupta1707@gmail.com>
2026-09-14 23:21:27 +0530

    Time: 14 ms (8.28%) | Memory: 46.5 MB (80.73%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+class Solution {
+    public int minimizedStringLength(String s) {
+        int count=0;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!st.contains(s.charAt(i))){
+                st.push(s.charAt(i));
+            }
+        }
+        return st.size();
+    }
+}
